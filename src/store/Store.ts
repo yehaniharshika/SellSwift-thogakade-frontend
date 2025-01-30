d077f7a7ae4a1e7edb501394f4441a433e8139f9
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-01-30 23:29:33 +0530

    create Store.ts file with adding CustomerSlice.ts

---
+++ src/store/Store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CustomerSlice from "../reducers/CustomerSlice.ts";
+
+
+const store = configureStore({
+    reducer: {
+        customers: CustomerSlice,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
